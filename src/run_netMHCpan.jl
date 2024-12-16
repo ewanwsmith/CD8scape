931508805197c14d25013c01b6dfa6c175c78391
Ewan Smith <e.smith.5@research.gla.ac.uk>
2024-12-16 14:51:53 +0000

    add command line functionality
    Former-commit-id: 988399ee95af51604735b197a435ed3e42458d28

--- src/run_netMHCpan.jl
+++ src/run_netMHCpan.jl
@@ -1,23 +1,62 @@
+#!/usr/bin/env julia
+
 # run_netMHCpan.jl
 
-# Change directory to netMHCpan installation path
-cd("/Users/e.smith.5/Documents/PhD/Software/netMHCpan-4.1")
+using Base
 
-# Path to the file containing alleles, one per line
-alleles_file = "/Users/e.smith.5/Documents/PhD/CD8scape/data/RSV_example/alleles.txt"
+# Function to parse command-line arguments
+function parse_arguments()
+    args = Dict()
+    for (i, arg) in enumerate(ARGS)
+        if arg in ["--folder", "-f"]
+            args["folder"] = ARGS[i + 1]
+        end
+    end
+    if !haskey(args, "folder")
+        error("Usage: ./run_netMHCpan.jl --folder /path/to/data")
+    end
+    return args
+end
 
-# Read alleles from file and join them with commas
-allele_list = readlines(alleles_file)
-alleles = join(allele_list, ",")
+# Main function
+function main()
+    # Parse the arguments
+    args = parse_arguments()
+    folder_path = args["folder"]
 
-# Define other variable options
-xlsfile_path = "/Users/e.smith.5/Documents/PhD/CD8scape/data/RSV_example/netMHCpan_output.tsv"
+    # Paths to required files in the specified folder
+    alleles_file = joinpath(folder_path, "alleles.txt")
+    peptides_file = joinpath(folder_path, "Peptides.pep")
+    xlsfile_path = joinpath(folder_path, "netMHCpan_output.tsv")
 
-# Construct the command without the binding affinity option
-cmd = `./netMHCpan -p /Users/e.smith.5/Documents/PhD/CD8scape/data/RSV_example/Peptides.pep \
-    -xls \
-    -a $(alleles) \
-    -xlsfile $(xlsfile_path)`
+    # Check that required files exist
+    for file in [alleles_file, peptides_file]
+        if !isfile(file)
+            error("File $file not found. Please ensure it exists in the specified folder.")
+        end
+    end
 
-# Run the command
-run(cmd)
+    # Change directory to netMHCpan installation path
+    cd("/Users/e.smith.5/Documents/PhD/Software/netMHCpan-4.1")
+
+    # Read only the first column from the alleles file
+    allele_list = open(alleles_file) do file
+        [split(line, r"\s+")[1] for line in readlines(file) if !isempty(line)]
+    end
+
+    # Join the alleles with commas
+    alleles = join(allele_list, ",")
+
+    # Construct the netMHCpan command
+    cmd = `./netMHCpan -p $(peptides_file) \
+        -xls \
+        -a $(alleles) \
+        -xlsfile $(xlsfile_path)`
+
+    # Run the command
+    println("Running netMHCpan with command:")
+    println(cmd)
+    run(cmd)
+end
+
+main()
