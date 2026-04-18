b60d6715112d71a7e9508f78ab0eeca8e9b3477a
cjri <christopher.illingworth@glasgow.ac.uk>
2026-04-18 07:28:29 +0100

    Initial test scripts

---
+++ test_script_1b_simulate.sh
@@ -0,0 +1,2 @@
+#Makes frames_simulated.csv and variants_simulated.csv: Describing set of all possible variants
+./CD8scape.jl simulate data/Example_data
---
+++ test_script_2_run.sh
@@ -0,0 +1,5 @@
+#Runs CD8scape for example variants
+./CD8scape.jl run data/Example_data
+
+#Outputs are describes in line 199 of README.md
+
