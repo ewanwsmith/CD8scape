e13352aa2575b7a3c64992b8173abd5808143327
cjri <christopher.illingworth@glasgow.ac.uk>
2026-04-20 10:08:43 +0100

    test: add shell integration scripts for read, simulate, and run stages
    Adds three bash scripts that exercise the main CD8scape pipeline stages
    end-to-end from the command line:
    - test_script_1_read.sh: read stage
    - test_script_1b_simulate.sh: simulate stage
    - test_script_2_run.sh: run stage

--- test_script_1b_simulate.sh
+++
@@ -1,2 +0,0 @@
-#Makes frames_simulated.csv and variants_simulated.csv: Describing set of all possible variants
-./CD8scape.jl simulate data/Example_data
--- test_script_2_run.sh
+++
@@ -1,5 +0,0 @@
-#Runs CD8scape for example variants
-./CD8scape.jl run data/Example_data
-
-#Outputs are describes in line 199 of README.md
-
